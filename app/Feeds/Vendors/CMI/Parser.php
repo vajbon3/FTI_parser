c9e0931245c3bbec8f47944464612722ef98bf03
vajaboy <v_chelidze2@cu.edu.ge>
2021-10-05 17:14:50 +0400

    CMI ver 4

--- app/Feeds/Vendors/CMI/Parser.php
+++ app/Feeds/Vendors/CMI/Parser.php
@@ -29,26 +29,26 @@ class Parser extends HtmlParser
 
     public function beforeParse(): void
     {
-        $this->description = $this->getHtml("#tbmore_info");
+        $this->description = $this->getHtml('#tbmore_info');
 
         // ишем стринг размеров
         $matches = [];
         if (preg_match("/\d*((\.\d+)|(\s+\d+\/\d+)*)(\"|')+[a-zA-z\s]*x[a-zA-z\s]*\d*((\.\d+)|(\s+\d+\/\d+)*)(\"|')+([a-zA-z\s]*x[a-zA-z\s]*\d*((\.\d+)|(\s+\d+\/\d+)*)(\"|')+)*/",$this->description,$matches)) {
-            $this->dims = FeedHelper::getDimsInString($matches[0],"x");
+            $this->dims = FeedHelper::getDimsInString($matches[0],'x');
         }
     }
 
     public function getProduct(): string
     {
-        return preg_replace( '/[^a-z0-9\s]/i', '', $this->getText("h1.title"));
+        return preg_replace( '/[^a-z0-9\s]/i', '', $this->getText('h1.title'));
     }
 
     public function getMpn(): string
     {
         try {
-            return trim(explode(":",$this->getText("h1.title > small"))[1]);
+            return trim(explode(':',$this->getText('h1.title > small'))[1]);
         } catch (\Exception) {
-            return "";
+            return '';
         }
     }
 
@@ -56,9 +56,9 @@ class Parser extends HtmlParser
     {
         $features = [];
 
-        $this->filter(".product-body div")->each(static function(ParserCrawler $c) use(&$features) {
-            if(stripos($c->getText("span"), "feature") !== false) {
-               $features = $c->nextAll()->getContent("li");
+        $this->filter('.product-body div')->each(static function(ParserCrawler $c) use(&$features) {
+            if(stripos($c->getText('span'), 'feature') !== false) {
+               $features = $c->nextAll()->getContent('li');
            }
         });
 
@@ -72,40 +72,38 @@ class Parser extends HtmlParser
 
     public function getDimX(): ?float
     {
-        return $this->dims["x"];
+        return $this->dims['x'];
     }
 
     public function getDimY(): ?float
     {
-        return $this->dims["y"];
+        return $this->dims['y'];
     }
 
     public function getDimZ(): ?float
     {
-        return $this->dims["z"];
+        return $this->dims['z'];
     }
 
     public function getCategories(): array
     {
-        $categories = $this->filter("div.breadcrumb a")->each(static fn(ParserCrawler $c) => $c->text());
-        $categories = array_slice($categories,0,count($categories)/2);
-        return array_slice($categories,0,5);
+          return array_slice(array_values( array_unique($this->getContent('div.breadcrumb a'))),0,5);
     }
 
     public function getAvail(): ?int
     {
-        return $this->exists("div.product-out-of-stock") ? 0 : self::DEFAULT_AVAIL_NUMBER;
+        return $this->exists('div.product-out-of-stock') ? 0 : self::DEFAULT_AVAIL_NUMBER;
     }
 
     public function getImages(): array
     {
-        $exampleImage = $this->getAttr(".thumb-view a","href");
-        return $exampleImage !== "" ? [$exampleImage] : [$this->getAttr(".main-product-image a","href")];
+        $exampleImage = $this->getAttr('.thumb-view a','href');
+        return $exampleImage !== '' ? [$exampleImage] : [$this->getAttr('.main-product-image a','href')];
     }
 
     public function isGroup(): bool
     {
-        return $this->getAvail() !== 0 && $this->getMpn() !== "";
+        return $this->getAvail() !== 0 && $this->getMpn() !== '';
     }
 
     public function getChildProducts(FeedItem $parent_fi): array
@@ -113,47 +111,52 @@ class Parser extends HtmlParser
         $child = [];
 
         // id продукта
-        $nid = $this->getAttr("input[name='nid']","value");
+        $nid = $this->getAttr("input[name='nid']",'value');
 
         // ключ атрибута
-        $attribute_key = $this->getAttr("select.color-select-picker","name");
+        $attribute_key = $this->getAttr('select.color-select-picker','name');
 
         // id цвет и имена
-        $colors = $this->filter("select.color-select-picker option")->each(static fn(ParserCrawler $c) => ["id" => $c->attr("value"), "name" => $c->text()]);
+        $colors = $this->filter('select.color-select-picker option')->each(static fn(ParserCrawler $c) => ['id' => $c->attr('value'), 'name' => $c->text()]);
 
         // размеры
         $sizes = [];
-        $this->filter(".input-qty-cell")->each(static function(ParserCrawler $c) use(&$sizes) {
-            $sizes[$c->attr("data-oid")] = $c->closest(".size-field")->getText(".size-label");
+        $this->filter('.input-qty-cell')->each(static function(ParserCrawler $c) use(&$sizes) {
+            $sizes[$c->attr('data-oid')] = $c->closest('.size-field')->getText('.size-label');
         });
 
         foreach($colors as $color) {
             $params = [
-                $attribute_key => $color["id"],
-                "nid" => $nid,
-                "qty" => 1,
+                $attribute_key => $color['id'],
+                'nid' => $nid,
+                'qty' => 1,
             ];
 
-            $links = [new Link("https://www.blankstyle.com/cart/add-to-cart-form/$nid","POST",$params, "form-data")];
+            $links = [new Link("https://www.blankstyle.com/cart/add-to-cart-form/$nid",'POST',$params, 'form-data')];
 
             foreach ($this->getVendor()->getDownloader()->fetch($links) as $data) {
                 $product_data = $data->getJSON();
 
                 foreach($product_data as $size_id => $info) {
-                    if($size_id === "otherDetail") {
+                    if($size_id === 'otherDetail') {
                         continue;
                     }
 
                     $fi = clone $parent_fi;
-                    $fi->setMpn("color:" . $color["name"] . ",size:" . $sizes[$size_id]);
-                    $fi->setCostToUs(StringHelper::getMoney($info["price"]));
-                    $fi->setListPrice(StringHelper::getMoney($info["MSRP"]));
-                    $fi->setRAvail((int)$info["inventory"]);
+
+                    $color_name = $color['name'];
+                    $color_id = $color['id'];
+                    $size_name = $sizes[$size_id];
+
+                    $fi->setProduct("Color:$color_name, Size:$size_name");
+                    $fi->setMpn($this->getMpn() . '-' . $color_id . '-' . $size_id);
+                    $fi->setCostToUs(StringHelper::getMoney($info['price']));
+                    $fi->setListPrice(StringHelper::getMoney($info['MSRP']));
+                    $fi->setRAvail((int)$info['inventory']);
 
                     // изображения
-                    $color_id = $color["id"];
                     $fi->setImages($this->filter(".thumb-view a[data-oid='$color_id']")
-                                ->each(static fn(ParserCrawler $c) => $c->attr("href")));
+                                ->each(static fn(ParserCrawler $c) => $c->attr('href')));
 
                     $child[] = $fi;
                 }
--- app/Feeds/Vendors/CMI/Vendor.php
+++ app/Feeds/Vendors/CMI/Vendor.php
@@ -13,13 +13,13 @@ class Vendor extends SitemapHttpProcessor
 
     protected const CHUNK_SIZE = 30;
 
-    protected array $first = [ "https://www.blankstyle.com/sitemap.xml" ];
+    protected array $first = [ 'https://www.blankstyle.com/sitemap.xml' ];
 
     public function filterProductLinks( Link $link ): bool
     {
-        $url = explode(".com/",$link->getUrl())[1];
+        $url = explode('.com/',$link->getUrl())[1];
 
-        if(str_contains($url,"article")) {
+        if(str_contains($url,'article')) {
             return false;
         }
 
@@ -31,6 +31,6 @@ class Vendor extends SitemapHttpProcessor
 
     public function isValidFeedItem(FeedItem $fi ): bool
     {
-        return !($fi->getMpn() === "" && !$fi->isGroup());
+        return !($fi->getMpn() === '' && !$fi->isGroup());
     }
 }
